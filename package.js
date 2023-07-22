e66955de2dce1d35c908b6a8a786e8b5f7cae1df
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-22 09:54:52 +0700

    update dependency

--- package.js
+++ package.js
@@ -1,4 +1,8 @@
 import { sayHello, sum } from 'learn_create_library_nodejs';
+import { max, min } from 'learn_create_library_nodejs/number';
 
 console.log(sayHello('John'));
 console.log(sum([1, 2, 3, 4, 5]));
+
+console.log(max(10 , 20));
+console.log(min(10 , 20));
